47b1e028b05514ffba10efbd142e4445832330ba
Ghaidaamoh <ghaidaa97mohammad@gmail.com>
2021-09-04 18:53:25 +0300

    finish lab13

--- src/models/vendor/vendor.js
+++ src/models/vendor/vendor.js
@@ -1,24 +1,36 @@
 "use strict";
 
-const clientio=require('socket.io-client')
-const host ='http://localhost:3000/caps'
-const connectcapSpace=clientio.connect(host)
-const faker= require('faker')
- 
-setInterval(() => {
-    let payload = {
-      store: "Ghaidaa",
-      orderID: faker.datatype.uuid(),
-      customer: faker.name.findName(),
-      address: faker.address.streetAddress(),
-    };
-  
-    connectcapSpace.emit('pickup', payload);
-  }, 5000);
-  
+const io =require('socket.io-client')
+const host= 'http://localhost:3000/caps'
+const connectcapSpace=io.connect(host)
+const faker = require("faker");
 
-  connectcapSpace.on('delivered', (payload) => {
-      console.log(`thank you for delivering ${payload.orderID}`);
-      
-    });
+connectcapSpace.on("delivered",(payload)=>{
+  console.log(`VENDOR: Thank you for delivering  ${payload.orderId}`);
+  
+})
 
+
+
+
+
+setInterval(()=>{
+
+    let order={
+        store:"Ghaidaa",
+        orderId:faker.datatype.uuid(),
+        customer:faker.name.findName(),
+        address:faker.address.streetAddress(),
+       
+    }
+    connectcapSpace.emit('pickup',order)
+    
+    
+
+},5000)
+
+connectcapSpace.on('orderinQueue',ordersQueue)
+function ordersQueue(payload){
+  console.log(`VENDOR: Order in Queue :`, payload.orderId);
+
+}
